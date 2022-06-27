1568ceba5c7d6592cf88999bfc5c971cc3bf693f
Sybren Dijkstra <nerbiz@gmail.com>
2022-06-27 14:54:58 +0200

    Fixed: file contents got mixed up during file renaming

--- src/Controllers/ReorderMigrationsController.php
+++ src/Controllers/ReorderMigrationsController.php
@@ -18,6 +18,12 @@ class ReorderMigrationsController extends Controller
      */
     protected Filesystem $disk;
 
+    /**
+     * The regular expression to match the date-prefix with
+     * @var string
+     */
+    protected string $dateRegEx = '/^(?<date>\d{4}_\d{2}_\d{2}_\d{6})/';
+
     public function __construct()
     {
         // Make a disk for the migrations directory
@@ -44,16 +50,15 @@ class ReorderMigrationsController extends Controller
      */
     public function processReorder(PreviewChangesRequest $request): RedirectResponse
     {
-        $dateRegEx = '/^(?<date>\d{4}_\d{2}_\d{2}_\d{6})/';
-        $submittedFilenames = $request->input('filenames');
         $currentFilenames = $this->disk->files();
+        $submittedFilenames = $request->input('filenames');
         $newFilenames = [];
 
         // Create the list of dates/prefixes to use
         if ($request->input('naming_mode') === 'current') {
             // Get the dates from all current files
-            $fileDates = array_map(function (string $filename) use ($dateRegEx) {
-                preg_match($dateRegEx, $filename, $matches);
+            $fileDates = array_map(function (string $filename) {
+                preg_match($this->dateRegEx, $filename, $matches);
                 return $matches['date'];
             }, $currentFilenames);
 
@@ -63,7 +68,7 @@ class ReorderMigrationsController extends Controller
                     // Get the file date for the current position
                     $fileDates[$index],
                     // Get the filename without the date part
-                    preg_replace($dateRegEx, '', $filename)
+                    preg_replace($this->dateRegEx, '', $filename)
                 );
             }
         } else {
@@ -75,7 +80,7 @@ class ReorderMigrationsController extends Controller
                     // Make a 6-digit counter string, like 000001
                     str_pad($index + 1, 6, '0', STR_PAD_LEFT),
                     // Get the filename without the date part
-                    preg_replace($dateRegEx, '', $filename)
+                    preg_replace($this->dateRegEx, '', $filename)
                 );
             }
         }
@@ -109,8 +114,29 @@ class ReorderMigrationsController extends Controller
         $currentFilenames = $request->input('current_filenames');
         $newFilenames = $request->input('new_filenames');
 
-        foreach ($currentFilenames as $index => $filename) {
-            $this->disk->move($filename, $newFilenames[$index]);
+        // Create an array of file names, with current date prefix
+        $files = [];
+        foreach ($currentFilenames as $filename) {
+            // Get the prefix and name from the filename
+            preg_match($this->dateRegEx, $filename, $matches);
+            $name = preg_replace($this->dateRegEx, '', $filename);
+            $files[$name] = ['current' => $matches['date']];
+        }
+
+        // Add the new date prefixes to the array
+        foreach ($newFilenames as $filename) {
+            // Get the prefix and name from the filename
+            preg_match($this->dateRegEx, $filename, $matches);
+            $name = preg_replace($this->dateRegEx, '', $filename);
+            $files[$name]['new'] = $matches['date'];
+        }
+
+        // Rename the files
+        foreach ($files as $name => $prefixes) {
+            $this->disk->move(
+                $prefixes['current'] . $name,
+                $prefixes['new'] . $name
+            );
         }
 
         return redirect()
